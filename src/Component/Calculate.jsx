f5c5138ae07c3cd445d08f6c31dc8fb2a63dffbf
nsakib9 <sakibkhan207@gmail.com>
2022-01-03 16:43:44 +0600

    State update wit setState

--- src/Component/Calculate.jsx
+++ src/Component/Calculate.jsx
@@ -1,35 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Calculate extends Component {
-state ={
-    count: 0,
-    name: 'New Website',
-    img: 'https://cdn.pixabay.com/photo/2021/02/02/17/13/trees-5974614_640.jpg'
-}
-style ={
+  state = {
+    count: 1,
+    name: "New Website",
+  };
+  style = {
     color: "red",
     fontSize: 20,
+  };
+
+  // renderTags(){
+  //     if(this.state.tags.length === 0) return <p>There are no tags now</p>;
+  //     return <ul>
+  //     {this.state.tags.map((tag) => (
+  //       <li key={tag}>{tag}</li>
+  //     ))}
+  //   </ul>
+  // }
+
+
+  handleIncrement = (product) => {
+  
+    this.setState({count: this.state.count +5});
+
+  }
+
+  addIncrement = () => {
+    this.handleIncrement({id: 1})
+  };
+
+  render() {
+    let classes = this.badgeClasses();
+
+    return (
+      <div>
+        <p>Calculate {this.state.count}</p>
+        <p style={this.style} className={classes}>
+          {this.state.count}
+        </p>
+        <button onClick={this.addIncrement} className="btn btn-primary">Add</button>
+      </div>
+    );
+  }
+
+  badgeClasses() {
+    let classes = "m-2 badge bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
-    render() { 
-        return (
-            <div>
-                
-                <p>Calculate {this.state.count}</p>
-                <p 
-                style={this.style} 
-                className={this.formatCount()}>
-                    {this.state.count}
-                </p>
-                <img width="200px" src={this.state.img} alt={this.state.name} />
-            </div>
-        );
-    }
-
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "m-2 badge bg-warning" : "m-2 badge bg-primary";
-    }
-}
- 
 export default Calculate;
