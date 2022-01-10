5445c310ebb51e76389e2a544e1d4c62b704394a
nsakib9 <sakibkhan207@gmail.com>
2022-01-10 17:17:47 +0600

    Encapsulate, reset, all from props

--- src/Component/calculate.jsx
+++ src/Component/calculate.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 class Calculate extends React.Component {
-  state = {
-    value: this.props.value,
-  };
-  handleAdd = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  // };
+  // handleAdd = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
   render() {
     let classes = this.badgeClasses();
     return (
       <div>
         <span className={classes}>{this.formcount()}</span>
-        <button onClick={this.handleAdd} className="btn btn-primary m-2">
+        <button onClick={() => this.props.onAdd(this.props.counter)} className="btn btn-primary m-2">
           Add
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger m-2"
         >
           Delete
@@ -26,12 +26,12 @@ class Calculate extends React.Component {
   }
   badgeClasses() {
     let classes = "btn m-2 btn-";
-    classes += this.state.value === 0 ? "primary" : "warning";
+    classes += this.props.counter.value === 0 ? "primary" : "warning";
     return classes;
   }
 
   formcount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
--- src/Component/counters.jsx
+++ src/Component/counters.jsx
@@ -7,23 +7,39 @@ class Counters extends React.Component {
       { id: 1, value: 0 },
       { id: 2, value: 5 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 4, value: 0 }
     ],
   };
+  onAdd = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter)
+    counters[index] = {...counter}
+    counters[index].value++
+    this.setState({counters})
+  }
   handleDelete = (counterId) => {
     //   console.log ('delte clicked', counterId)
     const counters = this.state.counters.filter((c) => c.id != counterId);
     this.setState({ counters: counters });
   };
+  handleReset = () => {
+    const counters = this.state.counters.map( c => {
+      c.value =0;
+      return c;
+    });
+    this.setState({ counters: counters})
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleReset} className="btn btn-primary m-2">Reset</button>
         {this.state.counters.map((counter) => (
           <Calculate
-            id={counter.id}
-            key={counter.id}
-            value={counter.value}
+            
             onDelete={this.handleDelete}
+            onAdd={this.onAdd}
+            key={counter.id}
+            counter={counter}
           />
         ))}
       </div>
